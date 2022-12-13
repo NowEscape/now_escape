55064ffb7419fd86a439b8e7a9c9af84f7225c65
thisisdana49 <thisisdana49@gmail.com>
2022-12-13 12:00:55 +0900

    #4 Splash 화면 생성

--- now_escape/App.js
+++ now_escape/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Loading from './Loading';
+import Main from './Main'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  };
+  componentDidMount = async() => {
+    // 1000초가 1초
+    setTimeout(() => {this.setState({isLoading: false})}, 10000);
+  }
+
+  render() {
+    if(this.state.isLoading) {
+      return <Loading/>
+    }
+    else {
+      return <Main/>
+    }
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
