998af3acd3a87a84dfeb472be7787e76285fcfd6
이유진 <rooproop1111@naver.com>
2022-12-23 12:07:07 +0900

    add type css
    type 별 css 추가
    props rounded, active, canceled, height
    
    To do : size 별 로직 추가

--- now_escape/components/Button/button.js
+++ now_escape/components/Button/button.js
@@ -2,29 +2,40 @@ import React from 'react';
 import {Text, View, StyleSheet, Pressable} from 'react-native';
 
 export default function Button(props){
-    const {onPress, text = ''} = props;
+    const {onPress, text = '', active, rounded, canceled, height} = props;
     return(
-        <Pressable style={styles.button} onPress={onPress}>
-            <Text style={styles.text}>{text}</Text>
+        <Pressable style={[styles(active, height).button, rounded && styles(active, height).rounded, canceled && styles(active, height).canceled]} onPress={onPress}>
+            <Text style={[styles(active, height).text, canceled && styles(active, height).canceledText]}>{text}</Text>
         </Pressable>
     );
 }
 
-const styles = StyleSheet.create({
-        button: {
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-            height: '7%',
-            backgroundColor: 'rgb(234, 75, 155)',
-            borderRadius: '0pt'
-        },
-        text: {
-            textAlign: 'center',
-            fontFamily: 'Pretendard-Bold',
-            fontSize: 18,
-            letterSpacing: 1.62,
-            color: 'rgb(255, 255, 255)'
-        }
+const styles = (active, height) => StyleSheet.create({
+            button: {
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100%',
+                height: height,
+                backgroundColor: active?'rgb(234, 75, 155)':'rgb(218, 218, 218)'
+            },
+            text: {
+                textAlign: 'center',
+                fontFamily: 'Pretendard',
+                fontWeight: 'bold',
+                fontSize: 18,
+                letterSpacing: 1.62,
+                color: 'white'
+            },
+            rounded: {
+                borderRadius: '10pt'
+            },
+            canceled: {
+                borderRadius: '10pt',
+                backgroundColor: 'rgb(230, 230, 230)'
+            },
+            canceledText: {
+                color: 'rgb(0, 0, 0)',
+            }
+
 })
