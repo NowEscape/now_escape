91a705ea223ad6089785eec4d5f89f9bc4ed5dc2
thisisdana49 <thisisdana49@gmail.com>
2023-01-03 11:51:05 +0900

    add label.js
    label 컴포넌트 추가
    props 임시 설정

---
+++ now_escape/components/Label/label.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Text, View, StyleSheet, Pressable} from 'react-native'
+
+export default function Label(props) {
+  const {content='', height, borderRadius} = props;
+
+  return (
+    <View style={styles(height, borderRadius).container}>
+      <Text style={styles(height, borderRadius).text}>{content}</Text>
+    </View>
+  );
+}
+
+const styles = (height, borderRadius) => StyleSheet.create({
+  container: {
+    width: 130,
+    height: height,
+    borderRadius: borderRadius,
+    backgroundColor: 'rgba(234, 75, 155, 0.13)'
+  },
+  text: {
+    width: 130,
+    height: height,
+    fontFamily: 'Pretendard',
+    fontSize: 17,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    letterSpacing: 0.3,
+    textAlign: 'center',
+    color: '#000000'
+  }
+})
