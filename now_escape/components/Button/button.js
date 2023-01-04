ba39297da26b2874d2b82371bb6285bc9d9d0b3a
이유진 <rooproop1111@naver.com>
2023-01-04 15:46:42 +0900

    props에 width 추가

--- now_escape/components/Button/button.js
+++ now_escape/components/Button/button.js
@@ -2,40 +2,40 @@ import React from 'react';
 import {Text, View, StyleSheet, Pressable} from 'react-native';
 
 export default function Button(props){
-    const {onPress, text = '', active, rounded, canceled, height} = props;
+    const {onPress, text = '', active, rounded, canceled, height, width} = props;
     return(
-        <Pressable style={[styles(active, height).button, rounded && styles(active, height).rounded, canceled && styles(active, height).canceled]} onPress={onPress}>
-            <Text style={[styles(active, height).text, canceled && styles(active, height).canceledText]}>{text}</Text>
+        <Pressable style={[styles(active, height, width).button, rounded && styles(active, height, width).rounded, canceled && styles(active, height, width).canceled]} onPress={onPress}>
+            <Text style={[styles(active, height, width).text, canceled && styles(active, height, width).canceledText]}>{text}</Text>
         </Pressable>
     );
 }
 
-const styles = (active, height) => StyleSheet.create({
-            button: {
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: '100%',
-                height: height,
-                backgroundColor: active?'rgb(234, 75, 155)':'rgb(218, 218, 218)'
-            },
-            text: {
-                textAlign: 'center',
-                fontFamily: 'Pretendard',
-                fontWeight: 'bold',
-                fontSize: 18,
-                letterSpacing: 1.62,
-                color: 'white'
-            },
-            rounded: {
-                borderRadius: '10pt'
-            },
-            canceled: {
-                borderRadius: '10pt',
-                backgroundColor: 'rgb(230, 230, 230)'
-            },
-            canceledText: {
-                color: 'rgb(0, 0, 0)',
-            }
+const styles = (active, height, width) => StyleSheet.create({
+    button: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: width,
+        height: height,
+        backgroundColor: active?'rgb(234, 75, 155)':'rgb(218, 218, 218)'
+    },
+    text: {
+        textAlign: 'center',
+        fontFamily: 'Pretendard',
+        fontWeight: 'bold',
+        fontSize: 18,
+        letterSpacing: 1.62,
+        color: 'white'
+    },
+    rounded: {
+        borderRadius: '10pt'
+    },
+    canceled: {
+        borderRadius: '10pt',
+        backgroundColor: 'rgb(230, 230, 230)'
+    },
+    canceledText: {
+        color: 'rgb(0, 0, 0)',
+    }
 
 })
