0b00b9a3d70a821a03c002e139d3d22ec248223e
thisisdana49 <thisisdana49@gmail.com>
2023-01-05 02:39:36 +0900

    add style by type
    1. TouchableOpacity로 구현함
    2. main/search에 따른 스타일 적용
    3. text의 가로 중앙 정렬 실패하여 원인 찾는 중
    
    혹시 코드 보시고 3번의 해결법 아신다면 코멘트 부탁드립니다.

--- now_escape/components/Label/label.js
+++ now_escape/components/Label/label.js
@@ -1,32 +1,69 @@
-import React from 'react';
-import {Text, View, StyleSheet, Pressable} from 'react-native'
+import React, {Fragment} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native'
 
 export default function Label(props) {
-  const {content='', height, borderRadius, type} = props;
+  const {height, width, borderRadius, type='', icon, text=''} = props;
 
   return (
-    <View style={styles(height, borderRadius).container}>
-      <Text style={styles(height, borderRadius).text}>{content}</Text>
-    </View>
+    <Fragment>
+      {/* 타입별 스타일 및 이벤트 */}
+      {props.type == 'mainLabel' && (
+        <TouchableOpacity onPress={()=>alert('main label')}>
+          <View style={styles(width, height, borderRadius).main}>
+            <Text style={styles(width, height).text}>{props.text}</Text>            
+          </View>
+        </TouchableOpacity>   
+      )}
+      {props.type == 'searchLabel' && (
+        <TouchableOpacity onPress={()=>alert('search label')}>
+          <View style={styles(width, height, borderRadius).search}>
+            <Text style={styles(width, height).text}>{props.text}</Text>            
+          </View>
+        </TouchableOpacity>   
+      )}     
+    </Fragment>
   );
 }
 
-const styles = (height, borderRadius) => StyleSheet.create({
-  container: {
-    width: 130,
+const styles = (width, height, borderRadius) => StyleSheet.create({
+  main: {
+    width: width,
     height: height,
     borderRadius: borderRadius,
-    backgroundColor: 'rgba(234, 75, 155, 0.13)'
+    backgroundColor: 'rgba(234, 75, 155, 0.13)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  search: {
+    width: width,
+    height: height,
+    borderRadius: borderRadius,
+    backgroundColor: "#ffffff",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderColor: "#ffd2e6"
   },
   text: {
-    width: 130,
-    height: height,
-    fontFamily: 'Pretendard',
+    width: '100%',
+    height: '100%',
+    // fontFamily: 'Pretendard',
     fontSize: 17,
     fontWeight: 'bold',
     fontStyle: 'normal',
     letterSpacing: 0.3,
     textAlign: 'center',
-    color: '#000000'
+    color: '#000000',
+    // borderWidth: 2,
+    // borderRadius: 5,
+    // borderStyle: 'solid',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    textAlignVertical: 'bottom',
+    // alignSelf: 'center',
+    // alignContent: 'center'
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 })
