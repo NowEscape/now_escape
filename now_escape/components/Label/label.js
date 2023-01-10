3cb271a33c0b612dfb413a53cbd9b46234bc3bc2
thisisdana49 <thisisdana49@gmail.com>
2023-01-11 00:16:19 +0900

    Svg 라이브러리 추가 및 적용
    svg-splash-screen 라이브러리 추가하고 triangleDown icon 추가함

--- now_escape/components/Label/label.js
+++ now_escape/components/Label/label.js
@@ -1,22 +1,29 @@
-import React, {Fragment} from 'react';
+import React, {Fragment,useState} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native'
+import Svg from 'now_escape/assets/iconTriangleDown.js'
+
 
 export default function Label(props) {
   const {height, width, borderRadius, type='', icon, text=''} = props;
+  const style = styles(width, height, borderRadius);
+  const [show, setShow] = useState(true);
 
   return (
     <Fragment>
       {/* 타입별 스타일 및 이벤트 */}
       {props.type == 'mainLabel' && (
         <TouchableOpacity onPress={()=>alert('main label')}>
-          <View style={styles(width, height, borderRadius).main}>
-            <Text style={styles(width, height).text}>{props.text}</Text>            
+          <View style={style.main}>
+            <View style={style.textBox}>
+              <Text style={style.text}>{props.text}</Text>
+              { show && <Svg/> }
+            </View>
           </View>
         </TouchableOpacity>   
       )}
       {props.type == 'searchLabel' && (
         <TouchableOpacity onPress={()=>alert('search label')}>
-          <View style={styles(width, height, borderRadius).search}>
+          <View style={style.search}>
             <Text style={styles(width, height).text}>{props.text}</Text>            
           </View>
         </TouchableOpacity>   
@@ -33,19 +40,29 @@ const styles = (width, height, borderRadius) => StyleSheet.create({
     backgroundColor: 'rgba(234, 75, 155, 0.13)',
     display: 'flex',
     alignItems: 'center',
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   search: {
     width: width,
     height: height,
     borderRadius: borderRadius,
-    backgroundColor: "#ffffff",
-    borderStyle: "solid",
+    backgroundColor: '#ffffff',
+    borderStyle: 'solid',
     borderWidth: 2,
-    borderColor: "#ffd2e6"
+    borderColor: "#ffd2e6",
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  textBox: {
+    width: width,
+    height: height,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   text: {
-    width: '100%',
     lineHeight: 'props.height',
     // fontFamily: 'Pretendard',
     fontSize: 17,
@@ -54,5 +71,5 @@ const styles = (width, height, borderRadius) => StyleSheet.create({
     letterSpacing: 0.3,
     textAlign: 'center',
     color: '#000000',
-  }
+  },
 })
