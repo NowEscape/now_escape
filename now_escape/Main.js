e920ac0c165792bee2daebbd3d7b92022e1b2166
thisisdana49 <thisisdana49@gmail.com>
2023-01-23 12:40:35 +0900

    Fix: "label svg 추가"
    - comment에 따라 수정
    - arrow down svg 추가
    
    Ref: #35

--- now_escape/Main.js
+++ now_escape/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, StyleSheet } from "react-native";
+import Label from "./components/Label/label";
 
 export default class Loading extends React.Component {
   render() {
     return (
         <View style={styles.container}>
-          <Text>Main</Text>
+          {/* <Text>Main</Text> */}
+          <Label width={165} height={49} type="searchLabel" text="전체장르"></Label>
           <StatusBar style="auto" />
         </View>
     );
