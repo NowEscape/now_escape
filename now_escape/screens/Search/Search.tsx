73df375c8430631dfd361abcd13d94fb25c294c9
thisisdana49 <thisisdana49@gmail.com>
2023-01-25 15:46:34 +0900

    Fix "Search/Label 수정"
    - Search css  수정
    - Label 왼쪽 아이콘 추가/오른쪽 화살표 방향 /active시 색깔 수정
    
    To do
    - Label 클릭시 연결 되는 컴포넌트 열기
    - css 적용 안되는 부분 해결
    - (논의) Platform별 파일 구분
    - (논의) 레이아웃 배치 flexbox 여부
    
    Related to: #35

--- now_escape/screens/Search/Search.tsx
+++ now_escape/screens/Search/Search.tsx
@@ -11,7 +11,6 @@ import dateStore from "../../store/dateStore";
 import Date from "../../components/setting/Date/date";
 import Time from "../../components/setting/Time/time";
 
-import { Dimensions } from 'react-native';
 import {fonts, width, height} from '../../globalStyles'
 
 import ArrowBackSVG from '../../assets/iconArrowBack'
@@ -40,50 +39,62 @@ export default function Search(){
                 <SearchSVG/>
             </View>
             <View style={{
-                    justifyContent: 'flex-end',
-                    width: currentWidth*300,
-                    height: 1,
-                    backgroundColor: "#000000",
-                    paddingLeft: 100,
-                    alignContent: 'flex-end'
-                }}></View>
-            <Label
-                style={styles.firstLabel}
-                height={currentHeight*49}
-                width={currentWidth*341}
-                borderRadius={10}
-                type={"searchLabel"}
-                text={ String(date.getFullYear() + '.' + date.getMonth() + 1 + '.'+ date.getDate())}
-                open={()=>{setDateVisible(dateVisible)}}
-            />
+                width: currentWidth*300,
+                height: 1,
+                marginTop: 8,
+                marginLeft: 25,
+                backgroundColor: "#000000",
+            }}></View>
+
+            <View style={{marginTop: currentHeight*20}}>
+                <Label
+                    height={currentHeight*49}
+                    width={currentWidth*341}
+                    borderRadius={10}
+                    type={"searchLabel"}
+                    icon={'date'}
+                    text={ String(date.getFullYear() + '.' + date.getMonth() + 1 + '.'+ date.getDate())}
+                    open={()=>{setDateVisible(dateVisible)}}
+
+                />
+            </View>
             <Date/>
+
+            <View style={{marginTop: currentHeight*14}}>
             <Label
-                style={styles.secondLabel}
                 height={currentHeight*49}
                 width={currentWidth*341}
                 borderRadius={10}
                 type={"searchLabel"}
+                icon={'time'}
                 text={time + ' 이후'}
                 open={()=>{setTimeVisible(timeVisible)}}
             />
+            </View>
             <Time/>
-            <View style={styles.rowContainer}>
+            <View
+            style={{
+                marginTop: currentHeight*17,
+                flexDirection: 'row',
+                display: 'flex',
+                width: '100%',
+            }}>
                 <Label
-                    style={styles.thirdLabel}
                     height={currentHeight*49}
                     width={currentWidth*165}
                     borderRadius={10}
                     type={"searchLabel"}
                     text={genre}
                     open={()=>setIsGenreSettingOpen((prevState => !prevState))}
+                    arrow='true'
                 />
                 <Label
-                    style={styles.thirdLabel}
                     height={currentHeight*49}
                     width={currentWidth*165}
                     borderRadius={10}
                     type={"searchLabel"}
                     text={"서울 홍대"}
+                    arrow='true'
                 />
             </View>
             {isGenreSettingOpen === true ? <Genre search={true}/> : null}
@@ -93,16 +104,15 @@ export default function Search(){
                     bottom: currentHeight*17
                 }}
             >
-            <Button
-                text={'검색'}
-                active={true}
-                rounded={true}
-                canceled={false}
-                height={currentHeight*63}
-                width={currentHeight*341}
-            />                
-            </View>
-
+                <Button
+                    text={'검색'}
+                    active={true}
+                    rounded={true}
+                    canceled={false}
+                    height={currentHeight*63}
+                    width={currentHeight*341}
+                />                
+            </View> 
         </SafeAreaView>
     );
 }
@@ -110,30 +120,23 @@ export default function Search(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        display:'flex',
+        height: '100%',
         flexDirection:'column',
         width:'100%',
         alignItems: 'center',
         ...Platform.select({
             android:{},
-            ios:{}
+            ios:{
+                marginTop: currentHeight*59,
+            }
         })
     },
     searchInput:{
         display:'flex',
-        flexDirection:'row',
-        width: currentWidth*300,
+        width: currentWidth*250,
         height: currentHeight*30,
-        ...Platform.select({
-            android:{},
-            ios:{}
-        })
-    },
-    searchContainer:{
-        display:'flex',
-        flexDirection:'column',
-        width: currentWidth*300,
-        height: currentHeight*30,
+        marginLeft: currentWidth*20,
+        marginRight: currentWidth*20,
         ...Platform.select({
             android:{},
             ios:{}
@@ -141,42 +144,11 @@ const styles = StyleSheet.create({
     },
     rowContainer:{
         width: '100%',
-        display:'flex',
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent: 'space-between',
+        flexDirection: 'row',
+        alignItems: 'center',
         ...Platform.select({
             android:{},
-            ios:{
-                paddingLeft: currentWidth*17,
-                paddingRight: currentWidth*17
-            }
-        })
-    },
-    firstLabel:{
-        ...Platform.select({
-            android:{},
-            ios:{
-                marginTop: 20.7,
-                marginBottom: 14,
-            }
-        })
-    },
-    secondLabel:{
-        ...Platform.select({
-            android:{},
-            ios:{
-                // marginBottom: 17,
-            }
-        })
-    },
-    thirdLabel:{
-        ...Platform.select({
-            android:{},
-            ios:{
-                // height:49,
-                // marginTop: 20
-            }
+            ios:{}
         })
     }
 })
