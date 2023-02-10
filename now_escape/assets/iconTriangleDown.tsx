88fcea074c0a951fe1cc48a9e6033f1e039d7888
thisisdana49 <thisisdana49@gmail.com>
2023-02-10 11:53:35 +0900

    Fix: "label icon/text"
    - rigionSetting 화면 label padding값, svg 크기 수정
    - rigionSetting 화면 label 가이드텍스트 추가

--- now_escape/assets/iconTriangleDown.tsx
+++ now_escape/assets/iconTriangleDown.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Svg, { SvgXml, Path } from 'react-native-svg'
+import Svg, { SvgXml, Path, SvgProps } from 'react-native-svg'
 
 const logo = `
   const logo = 
-  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8">
+  <svg xmlns="http://www.w3.org/2000/svg" width="21" height="13" viewBox="0 0 14 8">
     <path d="M5.871 1.29a1.5 1.5 0 0 1 2.258 0l3.694 4.222A1.5 1.5 0 0 1 10.694 8H3.306a1.5 1.5 0 0 1-1.129-2.488z" transform="rotate(180 7 4)" style="fill:#ea4b9b"/>
   </svg>
 `
 
-const svg = () => {
-  const LogoSvg = () => <SvgXml xml={logo}/>;
+const svg = ({height}:SvgProps) => {
+  const LogoSvg = () => <SvgXml xml={logo} height={height}/>;
   return <LogoSvg/>
 }
 
